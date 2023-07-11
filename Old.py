ba33739d7127be45ae72430149be4653f15f1952
MR-KINGX <128972681+MR-KINGX@users.noreply.github.com>
2023-07-11 18:01:58 +0500

    Add files via upload

--- Old.py
+++ Old.py
@@ -868,7 +868,7 @@ class Main:
 				"x-fb-net-hni": str(random.randint(20000, 40000)), 
 				"x-fb-connection-quality": "EXCELLENT",
 				"x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
-				"user-agent": 'Mozilla/5.0 (Linux; Android 14; Sumsung Build/TP1A.601997.071; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/105.0.5782.220 Mobile Safari/537.36 [FB_IAB/FBIOS;FBAV/196.0.0.61.79;]
+				"user-agent": 'Mozilla/5.0 (Linux; Android 14; Sumsung Build/TP1A.601997.071; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/105.0.5782.220 Mobile Safari/537.36',}
 				"content-type": "application/x-www-form-urlencoded", 
 				"x-fb-http-engine": "Liger"
 			}
@@ -1299,7 +1299,7 @@ class Main:
 				"x-fb-net-hni": str(random.randint(20000, 40000)), 
 				"x-fb-connection-quality": "EXCELLENT",
 				"x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
-				"user-agent": 'Mozilla/5.0 (Linux; Android 4; Sumsung Build/JFXQQQ; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.6445.869 Mobile Safari/537.36 [FB_IAB/FBIOS;FBAV/440.0.0.80.77;]
+				"user-agent": 'Mozilla/5.0 (Linux; Android 4; Sumsung Build/JFXQQQ; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.6445.869 Mobile Safari/537.36',}
 				"content-type": "application/x-www-form-urlencoded", 
 				"x-fb-http-engine": "Liger"
 			}
